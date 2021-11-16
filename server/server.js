c580a36bc55de3d7888bad7f33ab2947b5be024c
Justin Cummings <justin.lewis.cummings@gmail.com>
2021-11-15 20:12:07 -0600

    added function to switch string of operator with actual operator symbol

--- server/server.js
+++ server/server.js
@@ -18,16 +18,40 @@ app.use(express.static('./server/public'));
 
 app.post('/result', (req, res) => {
     console.log('in route for /result', req.body);
-
-  
     //req.body is the same as the "data" object in the ajax call
     const answer = doMath (req.body.firstNum, req.body.operator, req.body.secondNum);
+    const operatorSymbol = getSymbol(req.body.operator)
     console.log('answer', answer)
-    expressionsRan.push({firstNum:req.body.firstNum, operator:req.body.operator, secondNum:req.body.secondNum, answer: answer});
+    const equationHistory = {
+        firstNum:req.body.firstNum, 
+        operator:operatorSymbol, 
+        secondNum:req.body.secondNum,
+        answer: answer}
+
+    expressionsRan.push(equationHistory);
     res.json({
         answer: answer,
     expressionsRan: expressionsRan});
 });
+
+function getSymbol (operator) {
+    switch (operator) {
+        case 'add':
+            return '+'
+            break;
+            case 'subtract':
+                return '-'
+                break;
+                case 'divide':
+                    return '/'
+                    break;
+                    case 'multiply':
+                        return '*'
+                        break;
+        default:
+            break;
+    }
+}
 
 const doMath = (firstNum, operator, secondNum) => {
     if (operator === "add") {
